9a892d41665d5bc87b58c83f7b897681b9e4aa90
Build <support@mindpowered.dev>
2021-01-06 09:00:22 +0000

    Version 0.0.2

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='mindpowered-reservedseating',
+    version='0.0.2',
+    author="Mind Powered Corporation",
+    author_email="support@mindpowered.dev",
+    license="CPAL-1.0",
+    url="https://mindpowered.dev/",
+    description="ReservedSeating",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['reservedseating'],
+    packages=['mindpowered_reservedseating'],
+    package_dir={'mindpowered_reservedseating': 'wrappers'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'mindpowered-maglev',
+    ],
+)
